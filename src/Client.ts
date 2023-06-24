7da6743e38a849aea5b927692d187a9e2d06a28f
Aboshxm2 <72500560+Aboshxm2@users.noreply.github.com>
2023-06-24 12:24:08 +0300

    Update to v14

--- src/Client.ts
+++ src/Client.ts
@@ -1,28 +1,28 @@
-import { REST } from '@discordjs/rest';
-import { RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord-api-types/v10';
-import Discord from 'discord.js';
+import Discord, { REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
 import { Command } from './models/Command';
 
 export class Client extends Discord.Client {
     public commands: Map<string, Command> = new Map();
 
     public async deployCommands() {
-        const rest = new REST({ version: '9' }).setToken(this.token as string);
+        const rest = new REST().setToken(this.token as string);
         try {
-            console.log('Started refreshing application (/) commands.');
-    
+            console.log(`Started refreshing application (/) commands.`);
+
             const newCommands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
             this.commands.forEach((command: Command) => {
                 newCommands.push(command.data.toJSON());
             })
 
-            await rest.put(
-                Routes.applicationCommands(this.user?.id as string),
+            // The put method is used to fully refresh all commands in the guild with the current set
+            const data = await rest.put(
+                Routes.applicationCommands(this.user!.id),
                 { body: newCommands },
             );
-    
-            console.log('Successfully reloaded application (/) commands.');
+
+            console.log(`Successfully reloaded application (/) commands.`);
         } catch (error) {
+            // And of course, make sure you catch and log any errors!
             console.error(error);
         }
     }
--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -5,7 +5,7 @@ module.exports = new class extends Event {
     name: string = "ready";
     once: boolean = true;
     async execute(client: Client): Promise<void> {
-        console.log(`Ready! Logged in as ${client.user?.tag}`);
+        console.log(`Ready! Logged in as ${client.user!.tag}`);
 
         client.deployCommands();
     }
